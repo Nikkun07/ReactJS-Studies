d4db109c8805503731bf9dd411ee0356234cf236
Nikkun <gabriel.nikkun@gmail.com>
2024-08-19 22:54:07 +0800

    Chapter 1 - Importing & Exporting

---
+++ chapter-1/JSRefresher/assets/scripts/app.js
@@ -0,0 +1,5 @@
+import { apiKey, message } from "./util.js";
+
+
+
+console.log(apiKey, message);
---
+++ chapter-1/JSRefresher/assets/scripts/util.js
@@ -0,0 +1,2 @@
+export const apiKey = "asawdfgsdfgweqw";
+export const message = 'I actually want to fuckin die.';
