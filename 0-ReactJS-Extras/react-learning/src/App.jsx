f06d241decd962de3f63ea91e8b9bc6e1945965f
Nikkun <gabriel.nikkun@gmail.com>
2024-08-20 23:51:25 +0800

    Moved finished scripts

--- 0-ReactJS-Extras/react-learning/src/App.jsx
+++ 0-ReactJS-Extras/react-learning/src/App.jsx
@@ -1,14 +1,10 @@
-import Header from './Header.jsx';
-import Footer from './Footer.jsx';
-import Names from './Names.jsx';
+
 
 function App() {
 
   return(
     <>
-    <Header/>
-    <Names/>
-    <Footer/>
+    
     </>
   );
 }
